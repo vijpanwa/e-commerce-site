eb31822c46417301c0800b81cfdb897e12cd4ce5
Vijay.SinghPanwar <Vijay.SinghPawar@contractor.p66.com>
2023-04-30 22:17:20 +0530

    Add integration tests using test containers

--- product-service/src/main/java/com/example/productservice/dto/ProductResponse.java
+++ product-service/src/main/java/com/example/productservice/dto/ProductResponse.java
@@ -11,8 +11,10 @@ import java.math.BigDecimal;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ProductRequest {
+public class ProductResponse {
+    private String id;
     private String name;
     private String description;
     private BigDecimal price;
+
 }
