69c107701e95929b5da0ffdfb9480a346fe86d41
Vijay.SinghPanwar <Vijay.SinghPawar@contractor.p66.com>
2023-05-06 15:49:13 +0530

    Add eureka discovery server

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -39,8 +39,9 @@ public class OrderService {
                 .toList();
 
         // Check in inventory if the product is in stock.
-        InventoryResponseDto[] inventoryRespArr = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponseDto[] inventoryRespArr = webClientBuilder.build()
+                .get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
                 )
                 .retrieve()
